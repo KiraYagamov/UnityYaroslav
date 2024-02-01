3ff1f00cb5d25ec1fd6665eb8eb9c9ddd6e81d14
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2024-02-01 17:05:19 +0300

    asd

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,9 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private int _direction; // Переменная направления движения
+    private float _direction; // Переменная направления движения
     public float maxSpeed = 5f; // Переменная максимальной скорости
     private bool _isGrounded; // Переменная отвечающая за то, стоим ли мы на земле
     public float jumpForce = 5f; // Переменная силы прыжка
@@ -13,19 +14,7 @@ public class PlayerController : MonoBehaviour
     }
     void Update() // Выполняется каждый кадр
     {
-        if (Input.GetKey(KeyCode.D)) // Проверяем нажата ли клавиша D
-        {
-            _direction = 1; // Устанавливаем положительное направление
-        }
-        else if (Input.GetKey(KeyCode.A)) // Проверяем нажата ли клавиша A
-        {
-            _direction = -1; // Устанавливаем отрицательное направление
-        }
-        else // Проверяем, что клавиши A и D не нажаты
-        {
-            _direction = 0; // Устанавливаем направление на 0, то есть без движения
-        }
-
+        _direction = Input.GetAxis("Horizontal");
         if (Input.GetKey(KeyCode.Space) && _isGrounded) // Проверяем стоим ли мы на земле и нажата ли клавиша пробела
         {
             _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpForce); // Устанавливаем скорость по оси Y на силу прыжка
